5784da433f2a9471ee3e8549fa86f0633fd4f5a1
GenesisPresent <37956227+GenesisPresent@users.noreply.github.com>
2026-05-03 15:37:23 +0700

    Update author and project links in telegram.go

--- web/service/telegram.go
+++ web/service/telegram.go
@@ -220,7 +220,7 @@ func (s *TelegramService) StartRun() {
 		//	}
 		case "github":
 			msg.Text = `
-👩🏻‍💻 Here's the link to the project: https://github.com/NidukaAkalanka/x-ui-english/
+👩🏻‍💻 Here's the link to the project: https://github.com/GenesisPresent/x-ui-english/
              
 🖋 Author's Note on V0.2: 
 😶 My schedule is becoming tight so I may not be able to update the project frequently. I'm looking for a contributor who is familiar with Go Telegram Bot API, which is at https://go-telegram-bot-api.dev/ to further improve this Bot. (As you can feel, it's lacking the most user-friendly features like Buttons, Emojis...) If you are interested, please fork the repository and submit a pull request with your changes committed.`
@@ -234,10 +234,8 @@ func (s *TelegramService) StartRun() {
 💖Welcome to use the X-UI panel Telegram Bot! please send /help to see what can I do`
         case "author":
             msg.Text = `
-👦🏻 Author  : Niduka Akalanka
-📍 Github   : https://github.com/NidukaAkalanka
-📞 Telegram: @NidukaAkalanka (Contact for any issues. Please be patient. As I am a student, I may not be able to reply immediately.)
-📧 Email   : admin@itsmeniduka.engineer
+👦🏻 Author  : GenesisPresent
+📍 Github   : https://github.com/GenesisPresent
             `
 		default:
 			msg.Text = `⭐ X-UI 0.2 Telegram Bot Commands Menu ⭐
